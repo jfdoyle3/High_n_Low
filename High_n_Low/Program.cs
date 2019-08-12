2bf32fd9aeca2a86dd10f9f6efff0a40a12ea116
Jim Doyle <indellibleink@gmail.com>
2019-08-12 18:37:59 -0400

    Add project files.

---
+++ High_n_Low/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace High_n_Low
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerCard = 14;
+            int drawnCard = 2;
+
+            Console.WriteLine("Player Card: {0}",playerCard);
+            Console.WriteLine("Card Drawn: {0}", drawnCard);
+
+            if ( playerCard < drawnCard)
+                    Console.WriteLine("Win");
+            else if(playerCard>drawnCard)
+                    Console.WriteLine("Lose");
+            else if(playerCard==drawnCard)
+                    Console.WriteLine("Draw");
+
+
+
+            
+            
+        }
+    }
+}
