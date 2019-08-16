98e676d3e3136fba4e9bbe5a099659151dc05f4a
Jim Doyle <indellibleink@gmail.com>
2019-08-16 12:31:18 -0400

    remove deck class, removed unwanted comments and remove spaces clean the code up.

--- High_n_Low/Card.cs
+++ High_n_Low/Card.cs
@@ -8,30 +8,27 @@ namespace High_n_Low
     {
         public int Value;
         public int Suit;
-      //  public int Face;
-        // private string[] Suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+     // public int Face;
+     // private string[] Suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
         private string[] Suits = { "♠", "♥", "♣", "♦" };
-      //  private string[] Faces = { "J", "Q", "K", "A" };
+     // private string[] Faces = { "J", "Q", "K", "A" };
+
         public Card()
         {
             Random newCard = new Random();
             Random newSuit = new Random();
             this.Value = newCard.Next(2,15);
             this.Suit = newSuit.Next(0, 4);
-          //  if (this.Value>10)
-
         }
         
         public Card(int Value, int Suit)
         {
             this.Value = Value;
             this.Suit = Suit;
-
         }
 
         public string SuitSym()
         {
-
             return this.Suits[this.Suit];
         }
     }
--- High_n_Low/Game.cs
+++ High_n_Low/Game.cs
@@ -6,7 +6,7 @@ namespace High_n_Low
 {
    public class Game
     {
-       // public Card DealtCard= new Card();
+ 
 
         public Card DrawCard()
         {
@@ -14,37 +14,31 @@ namespace High_n_Low
            
             return aCard;
         }
-    public bool Compare (Card aCard, Card DealtCard)
+
+        public bool Compare (Card aCard, Card DealtCard)
         { 
             return (aCard.Value == DealtCard.Value && aCard.SuitSym() == DealtCard.SuitSym());
         }
-    public static void Play(string Guess, Card playerCard,Card drawnCard)
+
+        public static void Play(string Guess, Card playerCard,Card drawnCard)
         {
             Console.WriteLine("Your Card: {0}{1}",playerCard.Value,playerCard.SuitSym());
 
+
+            if ((Guess.ToLower()=="h") && (playerCard.Value>drawnCard.Value)|| (Guess.ToLower() == "l") && (playerCard.Value < drawnCard.Value))
+                Console.WriteLine("You Win" );
+            else if (playerCard.Value==drawnCard.Value)
+                Console.WriteLine("Draw");
+            else
+                Console.WriteLine("You Lose");   
         }
-    public string Guess()
+
+        public string Guess()
         {
-            Console.WriteLine("Is your card going to be (H)igher, (L)ower, (Q)uit");
+            Console.WriteLine("Is your card going to be (h)igher, (l)ower");
             string guess = Console.ReadLine();
 
             return guess;
-
         }
     }
 }
-
-
-
-
-//switch (Guess)
-//{
-//    case ((playerCard.Value < drawnCard.Value) && (Guess == "H" || Guess == "h" || Guess == "Higher" || Guess == "higher")):
-//        Console.WriteLine("Lose");
-//}
-//if ((playerCard.Value < drawnCard.Value) && (Guess == "H" || Guess == "h" || Guess == "Higher" || Guess == "higher"))
-//    Console.WriteLine("Lose");
-//else if ((playerCard.Value > drawnCard.Value) && (Guess == "L" || Guess == "l" || Guess == "Lower" || Guess == "Lower"))
-//    Console.WriteLine("Win");
-//else if (playerCard.Value == drawnCard.Value)
-//    Console.WriteLine("Draw");
